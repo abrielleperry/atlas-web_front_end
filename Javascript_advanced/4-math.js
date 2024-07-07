2974ea382ab3fdf4d58551a74ee2d593673f9614
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 18:37:44 -0500

    const not var

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -10,7 +10,7 @@ function addBy(firstNumber) {
   };
 }
 
-var addBy100 = addBy(100);
-var addBy1000 = addBy(1000);
-var divideBy10 = divideBy(10);
-var divideBy100 = divideBy(100);
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
