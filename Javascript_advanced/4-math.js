1c635e319c2779e1c1270d3ec9397fefc7ea49b1
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 18:36:31 -0500

    var add and divs

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -9,3 +9,8 @@ function addBy(firstNumber) {
     return firstNumber + secondNumber
   };
 }
+
+var addBy100 = addBy(100);
+var addBy1000 = addBy(1000);
+var divideBy10 = divideBy(10);
+var divideBy100 = divideBy(100);
