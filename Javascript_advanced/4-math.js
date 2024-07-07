7a040293ecca4714e1f9da236daa25f106cef626
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 18:47:51 -0500

    div second num by first num

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -1,12 +1,12 @@
 function divideBy(firstNumber) {
   return function (secondNumber) {
-    return firstNumber / secondNumber
+    return secondNumber / firstNumber;
   };
 }
 
 function addBy(firstNumber) {
   return function (secondNumber) {
-    return firstNumber + secondNumber
+    return firstNumber + secondNumber;
   };
 }
 
@@ -14,3 +14,9 @@ const addBy100 = addBy(100);
 const addBy1000 = addBy(1000);
 const divideBy10 = divideBy(10);
 const divideBy100 = divideBy(100);
+
+
+console.log(addBy100(20));
+console.log(divideBy10(20));
+console.log(divideBy100(200));
+console.log(addBy1000(20));
