f08ea4b3c93e3c462967bc619050ad75e0a23bc9
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 18:30:36 -0500

    add personal notes

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,13 +1,13 @@
-function createClassRoom(numbersOfStudents) {
-  function studentSeat(seat) {
+function createClassRoom(numbersOfStudents) { // create class room w specific number of students
+  function studentSeat(seat) { // nested func to assign seat to student
     return function () {
-      return seat;
+      return seat; // seat number
     };
   }
-  let students = [];
+  let students = []; // empty array to store student seat funcs
   for (let i = 0; i < numbersOfStudents; i++) {
-    students.push(studentSeat(i + 1));
+    students.push(studentSeat(i + 1)); // unique seat number pushed into students array and start at 1 not 0
   }
   return students;
 }
-let classRoom = createClassRoom(10);
+let classRoom = createClassRoom(10); // create class room w 10 students
