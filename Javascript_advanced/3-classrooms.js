2b8bb7d13961a4e58d3cfba918741c45d1c170ea
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 18:31:51 -0500

    another personal note

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -8,6 +8,6 @@ function createClassRoom(numbersOfStudents) { // create class room w specific nu
   for (let i = 0; i < numbersOfStudents; i++) {
     students.push(studentSeat(i + 1)); // unique seat number pushed into students array and start at 1 not 0
   }
-  return students;
+  return students; // return array of students seat numbers
 }
 let classRoom = createClassRoom(10); // create class room w 10 students
