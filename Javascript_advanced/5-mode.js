d6f72765d77409ea44d300388c503d6878e006f5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 19:53:55 -0500

    styles to page

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,4 +1,10 @@
 function changeMode(size, weight, transform, background, color) {
-
-
+  return function () {
+    document.body.style.fontSize = size + 'px';
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  };
 }
+
