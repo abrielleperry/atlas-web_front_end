53b526900ab645d4c49996b9f74c4379c852b4b5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 21:19:22 -0500

    orderid, amount

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -2,7 +2,7 @@ function processPayment(amount) {
   console.log('Collecting payment of ' + amount);
 }
 
-function processOrder(amount, orderId) {
+function processOrder(orderId, amount) {
   console.log(orderId + ' is being processed')
   processPayment(amount);
   console.log(orderId + ' has been fully processed')
