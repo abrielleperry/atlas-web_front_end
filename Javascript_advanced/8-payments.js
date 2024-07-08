1622f3ca264382e2fda0399273fdb0f273c277af
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 21:13:31 -0500

    declare func and call pp func

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -1,3 +1,10 @@
 function processPayment(amount) {
   console.log('Collecting payment of ' + amount);
 }
+
+function processOrder(amount, orderId) {
+  console.log(orderId + ' is being processed')
+  processPayment(amount);
+  console.log(orderId + ' has been fully processed')
+
+}
