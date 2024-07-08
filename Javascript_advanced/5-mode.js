ea85e34c6b44e86c6ffbb6f2d01618e56f571336
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 19:55:04 -0500

    main func w var modes

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -8,3 +8,8 @@ function changeMode(size, weight, transform, background, color) {
   };
 }
 
+function main() {
+  var spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  var darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  var screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+}
