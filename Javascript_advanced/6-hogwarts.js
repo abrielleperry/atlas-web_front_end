71d4e54c6b1d8ecd46cb5d169055ea81594475a0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 20:44:51 -0500

    puplic methods and constructor

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,8 +1,26 @@
-class studentHogwarts(privateScore, name) {
-  var privateScore = 0;
-  var name = null;
+class studentHogwarts {
+  constructor() {
+    var privateScore = 0;
+    var name = null;
 
-  const changeScoreBy = (points) => {
-    privateScore += points;
-  };
+    const changeScoreBy = (points) => {
+      privateScore += points;
+    };
+
+    this.setName = (newName) => {
+      name = newName;
+    };
+
+    this.rewardStudent = () => {
+      changeScoreBy(1);
+    };
+
+    this.penalizeStudent = () => {
+      changeScoreBy(-1);
+    };
+
+    this.getScore = () => {
+      return `${name}: ${privateScore}`;
+    };
+  }
 }
