3b211deac53a98cffa97654b7e5159478dec5710
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-08 12:41:15 -0500

    user obj w attrs

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,9 @@
+const user = {
+  hobby: Calligraphy,
+  favoriteSport: Hockey,
+  astrologicalSign: Aries,
+  firstName: Buillaume,
+  lastName: Johns,
+  location: Netherlands,
+  occupation: Engineer
+}
