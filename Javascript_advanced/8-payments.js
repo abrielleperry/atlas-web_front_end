1e7c2e349f677423987bdeb85f8f8a69a0add1ec
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-07 21:19:05 -0500

    ORDER ids and amounts

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -8,3 +8,10 @@ function processOrder(amount, orderId) {
   console.log(orderId + ' has been fully processed')
 
 }
+
+console.log('Processing orders')
+processOrder(12321, 10.99)
+processOrder(12322, 12.99)
+processOrder(12323, 15.0)
+
+console.log('All the orders have been processed')
