b9e87d7aff2dfe98e0dbc4159f72c4f65dc77699
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-08 12:05:13 -0500

    personl notes for check if i is prime

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -2,14 +2,20 @@ const { is } = require("@babel/types");
 
 function countPrimeNumbers() {
   let count = 0;
-  for (let i = 2; i < 100; i++) { // starts at 2 loop through 100 to check if prime 
-    if (isPrime(i)) {
-      count++;
+  for (let i = 2; i < 100; i++) { // starts at 2 loop through 100 
+    if (isPrime(i)) { // is number prime
+      count++; // if yes increment count
     }
   }
   return count;
 }
 
-function isPrime(num) {
+function isPrime(num) { // is given number prime
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) { // if num div by i wo remainder
+      return false; // not prime
+    }
+  }
+  return true; // prime
 
 }
