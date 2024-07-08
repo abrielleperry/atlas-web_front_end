2572531cdc01847e4ed2de6bf98340dd483becbf
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-08 13:19:58 -0500

    log first para

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -6,7 +6,15 @@ function createElement(data) {
 
 function queryWikipedia(callback) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*"', true);
-  xhr.onreadystatechange = function () { // call func when state changes
-  }
+  xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      var extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
+      callback(extract);
+    }
+  };
+  xhr.send();
 }
+
+queryWikipedia(createElement);
