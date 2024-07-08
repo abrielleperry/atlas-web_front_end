2417e93a078243a5003a052465bb6506da8cfc05
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-08 11:58:42 -0500

    if prime

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,6 +1,11 @@
+const { is } = require("@babel/types");
+
 function countPrimeNumbers() {
   let count = 0;
   for (let i = 2; i < 100; i++) {
-
+    if (isPrime(i)) {
+      count++;
+    }
   }
+  return count;
 }
