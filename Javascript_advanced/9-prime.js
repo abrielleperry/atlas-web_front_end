7f4a536c469a50dcc3a95e12a6db552042a83ead
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-08 12:01:46 -0500

    personal notes

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -2,10 +2,14 @@ const { is } = require("@babel/types");
 
 function countPrimeNumbers() {
   let count = 0;
-  for (let i = 2; i < 100; i++) {
+  for (let i = 2; i < 100; i++) { // starts at 2 loop through 100 to check if prime 
     if (isPrime(i)) {
       count++;
     }
   }
   return count;
+}
+
+function isPrime(num) {
+
 }
