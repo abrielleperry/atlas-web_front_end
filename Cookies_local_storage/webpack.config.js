32a9bc557bd2215224c19e946301654c881e2ec0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-07-12 13:34:49 -0500

    typo entries

--- Cookies_local_storage/webpack.config.js
+++ Cookies_local_storage/webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'), // This line is fine if you plan to use the dist directory later
   },
-  mode: 'development',
   devServer: {
-    static: path.join(__dirname, 'dist'),
+    static: [
+      path.join(__dirname, 'Cookies_local_storage') // Serve Cookies_local_storage directory
+    ],
     compress: true,
     port: 8080,
   },
